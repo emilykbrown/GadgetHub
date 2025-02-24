c2f15f6d6cd128452a358075757649d422bfb583
emilykbrown <disabledsarcasm@gmail.com>
2025-02-23 22:42:36 -0600

    RD01

--- GadgetHub.WebUI/Controllers/GadgetController.cs
+++ GadgetHub.WebUI/Controllers/GadgetController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using GadgetHub.Domain.Abstract;
+using GadgetHub.WebUI.Models;
 
 namespace GadgetHub.WebUI.Controllers
 {
@@ -11,14 +12,30 @@ namespace GadgetHub.WebUI.Controllers
     {
         private IGadgetRepository myrepospitory;
 
-        public GadgetController (IGadgetRepository gadgetRepository)
+        public GadgetController(IGadgetRepository gadgetRepository)
         {
             this.myrepospitory = gadgetRepository;
-        }   
+        }
 
-        public ViewResult List ()
+        public int PageSize = 4;
+
+        public ViewResult List(int page = 1)
         {
-            return View(myrepospitory.Gadgets);
+            GadgetsListViewModel model = new GadgetsListViewModel
+            {
+                Gadgets = myrepospitory.Gadgets.OrderBy(g => g.GadgetID)
+                                               .Skip((page - 1) * PageSize)
+                                               .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = myrepospitory.Gadgets.Count()
+                }
+
+            };
+        return View(model);
+
         }
     }
 }
---
+++ GadgetHub.WebUI/Models/GadgetsListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GadgetHub.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GadgetHub.WebUI.Models
+{
+    public class GadgetsListViewModel
+    {
+        public IEnumerable<Gadget> Gadgets { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
