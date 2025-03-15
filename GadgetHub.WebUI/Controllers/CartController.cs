369dae33d5df9527aae9d441912684cbc4fa68e2
emilykbrown <disabledsarcasm@gmail.com>
2025-03-14 23:48:36 -0500

    Add to cart table format

---
+++ GadgetHub.WebUI/Controllers/CartController.cs
@@ -0,0 +1,70 @@
+﻿using GadgetHub.Domain.Abstract;
+using GadgetHub.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GadgetHub.WebUI.Models;
+
+namespace GadgetHub.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IGadgetRepository repository;
+        public CartController (IGadgetRepository repo)
+        {
+            repository = repo;
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+
+            if (cart == null)
+            { 
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+
+        }
+
+        public RedirectToRouteResult AddToCart(int GadgetID, string returnUrl)
+        {
+            Gadget gadget = repository.Gadgets.FirstOrDefault
+                                                (g => g.GadgetID == GadgetID);
+
+            if (gadget != null)
+            {
+                GetCart().AddItem(gadget, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int GadgetID, string returnUrl)
+        {
+            Gadget gadget = repository.Gadgets.FirstOrDefault
+                                                (g => g.GadgetID == GadgetID);
+            
+            if (gadget != null)
+            {
+                GetCart().RemoveLine(gadget);
+            }
+            return RedirectToAction("Index", new {returnUrl});
+
+        }
+
+        public ViewResult Index(string returnURL)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnURL = returnURL
+            });
+        }
+    }
+
+}
