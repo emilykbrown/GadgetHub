bf4122867252b6f45aef36cc3c85c599dd027a8a
emilykbrown <disabledsarcasm@gmail.com>
2025-04-11 19:53:15 -0500

    Admin Index

---
+++ GadgetHub.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using GadgetHub.Domain.Abstract;
+
+namespace GadgetHub.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IGadgetRepository repository;
+
+        public AdminController(IGadgetRepository repo)
+        {
+           repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Gadgets);
+        }
+    }
+}
