0504e7bffbd872c8e3d2f98619c14ede71d6595c
emilykbrown <disabledsarcasm@gmail.com>
2025-04-11 21:16:20 -0500

    Editor datatypes

--- GadgetHub.Domain/Entities/Gadget.cs
+++ GadgetHub.Domain/Entities/Gadget.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace GadgetHub.Domain.Entities
 {
     public class Gadget
     {
+        [HiddenInput (DisplayValue = false)]
         public int GadgetID { get; set; }
         public string GadgetName { get; set; }
         public string GadgetBrand { get; set; }
+
+        [DataType(DataType.MultilineText)]
         public string GadgetDesc { get; set; }
         public string GadgetCategory { get; set; }
         public decimal GadgetPrice { get; set; }
